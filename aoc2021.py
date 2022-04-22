8292d6812b379fc5303e4f2316ecb4d0cae1bad3
StratenyNinja <erik.stvrtecky2000@gmail.com>
2022-04-22 19:16:40 +0200

    Update aoc2021.py

--- aoc2021.py
+++ aoc2021.py
@@ -2,6 +2,17 @@
 def day1():
     part1, part2 = 0, 0
 
+    with open("input_2021/day01.txt", "r") as f:
+        data = f.readlines()
+
+    previous_measurement = 0
+
+    for index in range(len(data)):
+        line = int(data[index])
+        if 0 < previous_measurement < line:
+            part1 += 1
+        previous_measurement = line
+    
     return part1, part2
 
 # ================================== ADVENT OF CODE TABLE ==================================
