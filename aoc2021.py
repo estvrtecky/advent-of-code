787b1b4512dda8a056236b0857a0d735d571729d
StratenyNinja <erik.stvrtecky2000@gmail.com>
2022-05-07 16:15:13 +0200

    Update aoc2021.py

--- aoc2021.py
+++ aoc2021.py
@@ -12,6 +12,28 @@ def day1():
             part2 += 1
 
     return part1, part2
+
+# ========================================= DAY 02 =========================================
+def day2():
+    part1, part2 = 0, 0
+
+    with open("input_2021/day02.txt", "r") as f:
+        data = [line.strip() for line in f]
+
+    horizontal_position = 0
+    depth = 0
+
+    for line in data:
+        command, number = line.split()
+        if command == "forward":
+            horizontal_position += int(number)
+        elif command == "up":
+            depth -= int(number)
+        elif command == "down":
+            depth += int(number)
+    
+    part1 = horizontal_position * depth
+
     return part1, part2
 
 # ================================== ADVENT OF CODE TABLE ==================================
@@ -21,3 +43,4 @@ if __name__ == "__main__":
     print(f"║        ║ {'PART 1':>15} ║ {'PART 2':>15} ║")
     print(f"╠════════╬{'═' * 17}╬{'═' * 17}╣")
     print(f"║ DAY 01 ║ {day1()[0]:>15} ║ {day1()[1]:>15} ║")
+    print(f"║ DAY 02 ║ {day2()[0]:>15} ║ {day2()[1]:>15} ║")
