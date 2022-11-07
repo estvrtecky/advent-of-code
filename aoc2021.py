4a814339f8c738aa7ae6f745618bc6952dbd9fa8
StratenyNinja <erik.stvrtecky2000@gmail.com>
2022-11-07 21:33:00 +0100

    day 02

--- aoc2021.py
+++ aoc2021.py
@@ -14,7 +14,30 @@ def day_01():
     return f"║ DAY 01 ║ {part1:>15} ║ {part2:>15} ║"
 
 # ========================================= DAY 02 =========================================
+def day_02():
     part1, part2 = 0, 0
+
+    with open("input_2021/day_02.txt", "r") as f:
+        data = [str(line.strip()) for line in f]
+
+    horizontal_position, depth_1, depth_2, aim = 0, 0, 0, 0
+
+    for command in data:
+        direction, units = command.split(" ")[0], int(command.split(" ")[1])
+        if direction == "forward":
+            horizontal_position += units
+            depth_2 += aim * units
+        elif direction == "up":
+            depth_1 -= units
+            aim -= units
+        elif direction == "down":
+            depth_1 += units
+            aim += units
+
+    part1 = horizontal_position * depth_1
+    part2 = horizontal_position * depth_2
+
+    return f"║ DAY 02 ║ {part1:>15} ║ {part2:>15} ║"
 
 
 
@@ -26,4 +49,5 @@ if __name__ == "__main__":
     print(f"╔════════╦{'═' * 17}╦{'═' * 17}╗")
     print(f"║        ║ {'PART 1':>15} ║ {'PART 2':>15} ║")
     print(f"╠════════╬{'═' * 17}╬{'═' * 17}╣")
+    print(day_01())
     print(day_02())
