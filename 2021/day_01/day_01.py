0221aac07d8c4080a0d00378220cc3675c9b92d1
StratenyNinja <erik.stvrtecky2000@gmail.com>
2022-11-07 22:11:35 +0100

    Create day_01.py

---
+++ 2021/day_01/day_01.py
@@ -0,0 +1,13 @@
+part1, part2 = 0, 0
+
+with open("day_01_input.txt", "r") as f:
+    data = [int(line.strip()) for line in f]
+
+for i in range(len(data)):
+    if i >= 1 and data[i] > data[i-1]:
+        part1 += 1
+    if i >= 3 and data[i] + data[i-1] + data[i-2] > data[i-1] + data[i-2] + data[i-3]:
+        part2 += 1
+
+print("PART 1:", part1)
+print("PART 2:", part2)
