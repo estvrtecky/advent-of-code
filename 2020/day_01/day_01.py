4d1bed6cd6dd299e51ebed07da537d4efcc4408a
Erik Štvrtecký <93426382+StratenyNinja@users.noreply.github.com>
2022-11-18 14:45:56 +0100

    2020 day 1 optimization

--- 2020/day_01/day_01.py
+++ 2020/day_01/day_01.py
@@ -7,10 +7,8 @@ for num1 in data:
     for num2 in data:
         if num1 + num2 == 2020:
             part1 = num1 * num2
-        if part2 == 0:
-            for num3 in data:
-                if num1 + num2 + num3 == 2020:
-                    part2 = num1 * num2 * num3
+        if 2020 - num1 - num2 in data:
+            part2 = num1 * num2 * (2020 - num1 - num2)
 
 print('PART 1:', part1)
 print('PART 2:', part2)
