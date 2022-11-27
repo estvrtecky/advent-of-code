81b9544d4bae038d0de9f458d98ced53061b0407
StratenyNinja <erik.stvrtecky2000@gmail.com>
2022-11-27 14:12:09 +0100

    fixed day 04

--- 2020/day_04/day_04.py
+++ 2020/day_04/day_04.py
@@ -30,8 +30,16 @@ for passport in data:
             elif 'hgt' in field:
                 if field[-2:] == 'cm' and 150 <= int(field[4:-2]) <= 193 or field[-2:] == 'in' and 59 <= int(field[4:-2]) <= 76:
                     valid_fields += 1
-            elif 'hcl' in field
-                
+            elif 'hcl' in field:
+                list_check = [True for char in field[5:] if char in '0123456789abcdef']
+                if field[4] == '#' and len(list_check) == 6:
+                    valid_fields += 1
+            elif 'ecl' in field and field[4:] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
+                valid_fields += 1
+            elif 'pid' in field and len(field[4:]) == 9:
+                valid_fields += 1
+        if valid_fields == 7:
+            part2 += 1
 
 print('PART 1:', part1)
 print('PART 2:', part2)
