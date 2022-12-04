b1c68677ca0c49550d02d335453cd44450763d33
Erik Štvrtecký <93426382+StratenyNinja@users.noreply.github.com>
2022-12-04 17:33:30 +0100

    Day 3: Rucksack Reorganization

--- 2022/03_Rucksack_Reorganization/day03.py
+++ 2022/03_Rucksack_Reorganization/day03.py
@@ -2,18 +2,21 @@ part1, part2 = 0, 0
 
 with open('day03.txt', 'r') as f:
     data = [line.strip() for line in f]
-    rucksacks = [[r[:len(r)//2], r[len(r)//2:]] for r in data]
-    rucksacks_part2 = [data[i-3:i] for i in range(len(data)+1) if i >= 3 and i % 3 == 0]
-print(rucksacks_part2)
+
+rucksacks_p1 = [[r[:len(r)//2], r[len(r)//2:]] for r in data]
+rucksacks_p2 = [data[i-3:i] for i in range(0, len(data)+1, 3) if i >= 3]
 values = {letter: num+1 for num, letter in enumerate('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')}
 
-for r in rucksacks:
+for r in rucksacks_p1:
     for char in r[0]:
         if char in r[1]:
             part1 += values[char]
             break
 
-
+for rs in rucksacks_p2:
+    for char in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ':
+        if char in rs[0] and char in rs[1] and char in rs[2]:
+            part2 += values[char]
 
 
 print('PART 1:', part1)
