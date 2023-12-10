7227fc586e391b7ed55310af19f737c793e82fc8
StratenyNinja <erik.stvrtecky2000@gmail.com>
2023-12-10 10:45:20 +0100

    Day 2: Dive! - Python solution

--- 2021/02/solution.py
+++ 2021/02/solution.py
@@ -1,23 +1,23 @@
 part1, part2 = 0, 0
 
-with open('day02.txt', 'r') as f:
-    data = [line.strip().split(' ') for line in f]
+with open("input.txt", "r") as f:
+    data = [line.strip().split(" ") for line in f]
 
 horizontal_position, depth_1, depth_2, aim = 0, 0, 0, 0
 for command in data:
     direction, units = command[0], int(command[1])
-    if direction == 'forward':
+    if direction == "forward":
         horizontal_position += units
         depth_2 += aim * units
-    elif direction == 'up':
+    elif direction == "up":
         depth_1 -= units
         aim -= units
-    elif direction == 'down':
+    elif direction == "down":
         depth_1 += units
         aim += units
 
 part1 = horizontal_position * depth_1
 part2 = horizontal_position * depth_2
 
-print('PART 1:', part1)
-print('PART 2:', part2)
+print("PART 1:", part1)
+print("PART 2:", part2)
